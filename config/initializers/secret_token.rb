a8bede2d48c27647a7ee2bc8dba5461a34ec0eca
William Lee <github@william-lee.com>
2011-11-25 15:22:46 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DessertSwan::Application.config.secret_token = 'eb967e80665aa7340868769dfbd16c0159766a08449ff9089edfc6cdfb83a953c5ef8e58b9080449c2f16db8325b288380bd122448cadc082ef74257a60e56b0'
